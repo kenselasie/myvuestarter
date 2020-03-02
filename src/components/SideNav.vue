9525ae9ab45935f1ffc23d36c51c0c37a769e3d0
kenselasie <kenselasie>
2020-03-02 11:31:16 +0000

    fixes

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -21,13 +21,13 @@
           <!-- Page Content -->
           <div id="page-content-wrapper">
 
-            <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+            <nav class="navbar navbar-expand-sm navbar-light bg-success border-bottom">
               <i class="ri-menu-line ri-2x" id="menu-toggle"></i>
               
-              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span style="border: none; cursor: pointer;" class="navbar-toggler" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <!-- <span class="navbar-toggler-icon"></span> -->
-                <i class="ri-more-2-fill"></i>
-              </button>
+                <i class="ri-more-2-fill ri-xl"></i>
+              </span>
 
               <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
