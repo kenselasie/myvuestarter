7e6dc69647c48a142204374f8cd94af69654c6bf
kenselasie <kennedyadzoyi98@gmail.com>
2020-05-18 14:59:57 +0000

    added production path

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
+
+const webpack = require('webpack');
+const isProd = process.env.NODE_ENV === "production";
+
 module.exports = {
+  publicPath: isProd ? "/thepath" : "",
+   configureWebpack: {
+       // Set up all the aliases we use in our app.
+       plugins: [
+           new webpack.optimize.LimitChunkCountPlugin({
+               maxChunks: 6
+           })
+       ]
+   },
     css: {
       loaderOptions: {
         sass: {
