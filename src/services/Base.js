bed0d1c2f7e30d5754aad6e4334e410558a820be
kenselasie <kennedyadzoyi98@gmail.com>
2020-06-06 23:53:27 +0000

    added dotenv

--- src/services/Base.js
+++ src/services/Base.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 export default () => {
     return axios.create({
-        baseURL: 'http://157.230.235.122:23000/', 
+        baseURL: process.env.VUE_APP_BASE_URL, 
         // responseType: 'json',
         timeout: 15000,
         headers: {
